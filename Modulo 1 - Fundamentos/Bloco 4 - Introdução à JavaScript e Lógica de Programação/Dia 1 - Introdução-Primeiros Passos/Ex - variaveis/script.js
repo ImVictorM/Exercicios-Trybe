a90304e5fc7c6aa3d7ccf786f22ad946148bbdf4
Victor Mendes <victorutdsim7@gmail.com>
2022-06-21 15:48:07 -0300

    Exercicio de variável

---
+++ Modulo 1 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 1 - Introdução-Primeiros Passos/Ex - variaveis/script.js
@@ -0,0 +1,8 @@
+const myName = "Victor";
+const birthCity = "Limeira";
+let birthYear = 2001;
+birthYear = 2030;
+//birthCity = "São Paulo"; //erro por ser constante
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
