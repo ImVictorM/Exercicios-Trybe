641189b3d22cdea5a00143cfee77353d7f680ac4
Victor Mendes <victorutdsim7@gmail.com>
2022-06-22 19:10:51 -0300

    req 7 pratica

--- Modulo 1 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 2 - Array e loop for/Ex - pratica/scriipt.js
+++ Modulo 1 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 2 - Array e loop for/Ex - pratica/scriipt.js
@@ -1,26 +1,26 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
+
 //iteração cada numero e soma de todos os numeros
 for (eachNumber of numbers) {
     console.log(eachNumber);
     sum += eachNumber;
 }
-
-let average = sum / numbers.length
-
 console.log("Total: ", sum);
-console.log("Média: ", average);
 
+//media da lista
+let average = sum / numbers.length
+console.log("Média: ", average);
 if (average > 20) {
     console.log("valor maior que 20");
 } else {
     console.log("valor menor ou igual a 20");
 }
-let maxValue = 0;
 
 //valor maximo
+let maxValue = null;
 for (let index = 0; index < numbers.length; index += 1) {
-    if (maxValue === 0) {
+    if (maxValue === null) {
         maxValue = numbers[0];
     } else if (numbers[index] > maxValue) {
         maxValue = numbers[index];
@@ -28,15 +28,27 @@ for (let index = 0; index < numbers.length; index += 1) {
 }
 console.log("Maior valor da lista: ", maxValue);
 
+//contagem impares
 let oddCount = 0
 for (let index = 0; index < numbers.length; index += 1) {
     if (numbers[index] % 2 !== 0) {
         oddCount += 1;
     }
 }
-
 if (oddCount === 0) {
     console.log("Nenhum valor ímpar encontrado");
 } else {
     console.log("Quantidade de números ímpares: ", oddCount);
 }
+
+//valor minimo
+let minValue = null;
+for (let index = 0; index < numbers.length; index += 1) {
+    if (minValue === null) {
+        minValue = numbers[0];
+    } else if (numbers[index] < minValue) {
+        minValue = numbers[index];
+    }
+}
+console.log("Menor valor da lista: ", minValue);
+
