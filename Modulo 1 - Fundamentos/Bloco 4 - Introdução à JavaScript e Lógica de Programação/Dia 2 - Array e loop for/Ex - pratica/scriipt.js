220b178936704d2ac016f3422827fc5afc7e73a2
Victor Mendes <victorutdsim7@gmail.com>
2022-06-22 18:39:26 -0300

    req 2 pratica

--- Modulo 1 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 2 - Array e loop for/Ex - pratica/scriipt.js
+++ Modulo 1 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 2 - Array e loop for/Ex - pratica/scriipt.js
@@ -1,5 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
 
 for (eachNumber of numbers) {
     console.log(eachNumber);
+    sum += eachNumber;
 }
+console.log("Total: ", sum);
