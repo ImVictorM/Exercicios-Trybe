81dc78987b3dc4e7029eb9249781ed54397f69c5
Victor Mendes <victorutdsim7@gmail.com>
2022-06-22 18:57:25 -0300

    req 4 e 5 [foguetinho] pratica

--- Modulo 1 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 2 - Array e loop for/Ex - pratica/scriipt.js
+++ Modulo 1 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 2 - Array e loop for/Ex - pratica/scriipt.js
@@ -10,3 +10,19 @@ let average = sum / numbers.length
 
 console.log("Total: ", sum);
 console.log("Média: ", average);
+
+if (average > 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log("valor menor ou igual a 20");
+}
+let maxValue = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (maxValue === 0) {
+        maxValue = numbers[0];
+    } else if (numbers[index] > maxValue) {
+        maxValue = numbers[index];
+    }
+}
+console.log("Maior valor da lista: ", maxValue)
