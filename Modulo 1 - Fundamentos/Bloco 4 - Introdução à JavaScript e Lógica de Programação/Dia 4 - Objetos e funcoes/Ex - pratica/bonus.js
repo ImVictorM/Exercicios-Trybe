083e6deb9a32174152ca17dfcd19dc1f0ec6ae24
Victor Mendes <victorutdsim7@gmail.com>
2022-06-27 01:21:57 -0300

    refatoração exercicio algarismos romanos

--- Modulo 1 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 4 - Objetos e funcoes/Ex - pratica/bonus.js
+++ Modulo 1 - Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/Dia 4 - Objetos e funcoes/Ex - pratica/bonus.js
@@ -20,76 +20,84 @@ function converterAlgarismo(string) {
         }
     }
     //validação caracteres invalidos
-    if(valores.length < string.length){
+    if (valores.length < string.length) {
         return "[ERROR] um ou mais caracteres não pertencem a algarismos romanos."
     }
     //validação antecedentes
-    for(let index = 0; index < valores.length; index += 1) {
+    for (let index = 0; index < valores.length; index += 1) {
         let proximo = index + 1;
-        if(valores[index] === 1) {
-            if(valores[proximo] > 10){
+        if (valores[index] === 1) {
+            if (valores[proximo] > 10) {
                 return "[ERROR]";
             }
-        } else if(valores[index] === 10) {
-            if(valores[proximo] > 100) {
+        } else if (valores[index] === 5) {
+            if (valores[proximo] > 5) {
+                return "[ERROR]"
+            }
+        } else if (valores[index] === 10) {
+            if (valores[proximo] > 100) {
                 return "[ERROR]";
             }
         } else if (valores[index] === 50) {
-            if(valores[proximo] > 50) {
+            if (valores[proximo] > 50) {
                 return "[ERROR]";
             }
-        } 
+        } else if (valores[index] === 500) {
+            if (valores[proximo] > 500) {
+                return "[ERROR]";
+            }
+        }
+    }
+    //validação exceção numero 1
+    for (let index = 0; index < valores.length; index += 3) {
+        if (valores[index] === 1 && valores.length >= index + 3) {
+            return "[ERROR]";
+        }
     }
 
     //validação repetição de algarismos
-    for(let index = 0; index < valores.length; index += 1){
+    for (let index = 0; index < valores.length; index += 1) {
         let repeticoes = 0;
-        for(let valor of valores){
-            if(valores[index] === valor){
+        for (let secondIndex = 0; secondIndex < valores.length; secondIndex += 1) {
+            if (valores[index] === valores[secondIndex]) {
                 repeticoes += 1;
             }
-            if(repeticoes > 3){
+            if (repeticoes > 3) {
                 return "[ERROR] numero de repetições maximo de um algarismo excedido";
             }
         }
     }
+
     console.log(valores);
 
     // soma
     let valorCheio = null;
     let index = 0;
     while (index < valores.length) {
-
-        if (valores[index] > valores[index + 1]) {  //primeiro maior que segundo
-            if(valores[index + 2] > valores[index + 1]){
-                valorCheio += (valores[index] + (valores[index + 2] - valores[index + 1]));
-                index += 3;
-            } else if (valores[index + 1] === valores[index + 2] && valores[index + 1] === valores[index + 3]) {
-                valorCheio += valores[index];
-                valorCheio += (valores[index + 1] + valores[index + 2] + valores[index + 3]);
-                index += 4;
-            } else if (valores[index + 1] === valores[index + 2]) {
-                valorCheio += valores[index];
-                valorCheio += (valores[index + 1] + valores[index + 2]);
-                index += 3;
-            } else if (valores[index] === valores[index + 2]) {
-                valorCheio += ((valores[index + 2] - valores[index + 1]) + valores[index]);
-                index += 3
+        let proximo = index + 1;
+        if (valores[index] > valores[proximo]) {  //primeiro maior que segundo
+            if (valores.length >= index + 3) {
+                if (valores[proximo] > valores[proximo + 1]) {
+                    valorCheio += (valores[index] + (valores[proximo + 1] - valores[proximo]));
+                    index += 3;
+                } else {
+                    valorCheio += ((valores[proximo + 1] - valores[proximo]) + valores[index]);
+                    index += 3;
+                }
             } else {
-                valorCheio += (valores[index] + valores[index + 1]);
+                valorCheio += (valores[index] + valores[proximo]);
                 index += 2;
             }
-        } else if (valores[index] === valores[index + 1]) { //sequencia de iguais
-            if (valores[index + 1] === valores[index + 2]) {
-                valorCheio += (valores[index] + valores[index + 1] + valores[index + 2]);
+        } else if (valores[index] === valores[proximo]) { //sequencia de iguais
+            if (valores[proximo] === valores[proximo + 1]) {
+                valorCheio += (valores[index] + valores[proximo] + valores[proximo + 1]);
                 index += 3;
             } else {
-                valorCheio += (valores[index] + valores[index + 1]);
+                valorCheio += (valores[index] + valores[proximo]);
                 index += 2;
             }
-
-        } else if (valores[index] < valores[index + 1]) { //sucessor ser maior que anterior
-            valorCheio += (valores[index + 1] - valores[index]);
+        } else if (valores[index] < valores[proximo]) { //sucessor ser maior que anterior
+            valorCheio += (valores[proximo] - valores[index]);
             index += 2;
         } else { // proximo valor === undefined;
             valorCheio += valores[index];
@@ -99,11 +107,11 @@ function converterAlgarismo(string) {
 
     //validação valor maximo
     let valorMaximoPossivel = 3999;
-    if(valorCheio > valorMaximoPossivel){
+    if (valorCheio > valorMaximoPossivel) {
         return "[ERROR] valor maximo excedido";
     }
 
     return valorCheio;
 }
 
-console.log(converterAlgarismo("cccc"));
+console.log(converterAlgarismo("xxicc"));
