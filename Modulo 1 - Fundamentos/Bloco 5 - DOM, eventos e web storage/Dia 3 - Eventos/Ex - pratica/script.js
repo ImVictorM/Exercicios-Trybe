50878fb9a5d4eef612a95755dad20df12e7daff5
Victor Mendes <victorutdsim7@gmail.com>
2022-07-01 11:01:22 -0300

    exercicio pratica [foguetinho] requisito 3

--- Modulo 1 - Fundamentos/Bloco 5 - DOM, eventos e web storage/Dia 3 - Eventos/Ex - pratica/script.js
+++ Modulo 1 - Fundamentos/Bloco 5 - DOM, eventos e web storage/Dia 3 - Eventos/Ex - pratica/script.js
@@ -18,14 +18,14 @@ createDaysOfTheWeek();
 let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 let ul = document.getElementById('days');
-function holiday(number) {
+function isHoliday(number) {
   let holidayDays = [24, 25, 31];
   if (holidayDays.includes(number)) {
     return true;
   }
   return false;
 }
-function fridayzou(number) {
+function isFriday(number) {
   let friday = [4, 11, 18, 25];
   if(friday.includes(number)) {
     return true;
@@ -38,19 +38,31 @@ for (let index = 0; index < decemberDaysList.length; index += 1) {
   listItem.classList.add('day');
   ul.appendChild(listItem);
   listItem.innerText = decemberDaysList[index];
-  if (holiday(decemberDaysList[index])) {
+  if (isHoliday(decemberDaysList[index])) {
     listItem.classList.add('holiday');
   }
-  if (fridayzou(decemberDaysList[index])) {
+  if (isFriday(decemberDaysList[index])) {
     listItem.classList.add('friday');
   }
 }
 
-function feriados(string) {
-  let button = document.createElement('button');
+let button = document.createElement('button');
+function createHolidayButton(string) {
   button.classList.add('btn-holiday');
   button.innerText = string;
   document.getElementsByClassName('buttons-container')[0].appendChild(button);
 }
 
-feriados('Feriados');
+createHolidayButton('Feriados');
+
+button.addEventListener('click', function() {
+  let holidays = document.getElementsByClassName('holiday');
+  console.log(holidays);
+  for (let day of holidays) {
+    if (day.style.backgroundColor === 'rgb(250, 221, 221)') {
+      day.style.backgroundColor = 'rgb(238,238,238)';
+    } else {
+      day.style.backgroundColor = 'rgb(250, 221, 221)';
+    }
+  }
+});
