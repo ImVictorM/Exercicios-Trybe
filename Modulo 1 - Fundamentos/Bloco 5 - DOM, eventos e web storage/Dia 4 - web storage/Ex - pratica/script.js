aee8e5295fd534bab20b08a9f54c43258a810778
Victor Mendes <victorutdsim7@gmail.com>
2022-07-02 13:37:53 -0300

    finalização

--- Modulo 1 - Fundamentos/Bloco 5 - DOM, eventos e web storage/Dia 4 - web storage/Ex - pratica/script.js
+++ Modulo 1 - Fundamentos/Bloco 5 - DOM, eventos e web storage/Dia 4 - web storage/Ex - pratica/script.js
@@ -14,8 +14,17 @@ function changeFontColor() {
   document.body.style.color = colorInput;
 }
 
-function changeFontFamily() {
-  
+function changeFontFamily(event) {
+  switch(event.target.value) {
+    case 'times':
+      document.body.style.fontFamily = 'Times New Roman';
+      break;
+    case 'monospace':
+      document.body.style.fontFamily = 'monospace';
+      break;
+    case 'arial':
+      document.body.style.fontFamily = 'Arial';
+  }
 }
 
 window.onload = function() {
@@ -26,25 +35,22 @@ window.onload = function() {
   fontCOlorButton.addEventListener('click', changeFontColor);
 
   let selectFont = document.getElementById('font-select');
-  selectFont.addEventListener('click', function() {
-    switch(selectFont.value) {
-      case 'times':
-        document.body.style.fontFamily = 'Times New Roman';
-        break;
-      case 'monospace':
-        document.body.style.fontFamily = 'monospace';
-        break;
-      case 'arial':
-        document.body.style.fontFamily = 'Arial';
-    }
-  });
+  selectFont.addEventListener('click', changeFontFamily);
+
   let fontSizeField = document.getElementById('font-size-input');
   fontSizeField.addEventListener('click', function() {
     if (fontSizeField.value === '') {
       fontSizeField.value = 18;
     }
   });
-
+  fontSizeField.addEventListener('wheel', function(event){
+    if (event.target.value > 29) {
+      event.target.value = 29;
+    } else if (event.target.value < 19) {
+      event.target.value = 19;
+    }
+  });
+    
   let fontSizeButton = document.getElementById('font-size-button');
   fontSizeButton.addEventListener('click', function() {
     if (fontSizeField.value > 30) {
@@ -55,6 +61,33 @@ window.onload = function() {
       fontSizeField.value = 18;
     } else {
       document.body.style.fontSize = fontSizeField.value + 'px';
+    }
+  });
+
+  let lineSpacingField = document.getElementById('line-spacing-field');
+  lineSpacingField.addEventListener('click', function() {
+    if (lineSpacingField.value === '') {
+      lineSpacingField.value = 20;
+    }
+  });
+  lineSpacingField.addEventListener('wheel', function(event){
+    if (event.target.value > 59) {
+      event.target.value = 59;
+    } else if (event.target.value < 21) {
+      event.target.value = 21;
+    }
+  });
+
+  let lineSpacingButton = document.getElementById('line-spacing-button');
+  lineSpacingButton.addEventListener('click', function() {
+    if (lineSpacingField.value > 60) {
+      document.body.style.lineHeight = '60px';
+      lineSpacingField.value = 60;
+    } else if (lineSpacingField < 20) {
+      document.body.style.lineHeight = '20px';
+      lineSpacingField.value = 20;
+    } else {
+      document.body.style.lineHeight = lineSpacingField.value + 'px';
     }
   });
 }
