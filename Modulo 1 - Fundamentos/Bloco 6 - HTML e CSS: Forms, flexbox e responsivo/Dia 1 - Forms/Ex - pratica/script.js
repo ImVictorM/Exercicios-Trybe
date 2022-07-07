7a624ae9c32c13d5e065733e5c3987ca86cd3d28
Victor Mendes <victorutdsim7@gmail.com>
2022-07-07 18:50:15 -0300

    requisito bonus 2

--- Modulo 1 - Fundamentos/Bloco 6 - HTML e CSS: Forms, flexbox e responsivo/Dia 1 - Forms/Ex - pratica/script.js
+++ Modulo 1 - Fundamentos/Bloco 6 - HTML e CSS: Forms, flexbox e responsivo/Dia 1 - Forms/Ex - pratica/script.js
@@ -1,6 +1,32 @@
-const submitButton = document.getElementById('submit-button');
-submitButton.addEventListener('click', (event) => {
-  event.preventDefault();
+function nameValidation(name) {
+  if (name.length < 10 || name.length > 40) {
+    return false;
+  }
+  return true;
+}
+function emailValidation(email) {
+  if (email.length < 10 || email.length > 50) {
+    return false;
+  }
+  return true;
+}
+function textValidation(text) {
+  if (text.length > 500) {
+    return false;
+  }
+  return true;
+}
+const form = document.getElementById('form');
+form.addEventListener('submit', (event) => {
+  const name = document.getElementById('name-input').value;
+  const email = document.getElementById('email-input').value;
+  const text = document.getElementById('text-area').value;
+  if (nameValidation(name) && emailValidation(email) && textValidation(text)) {
+    window.alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+  } else {
+    event.preventDefault();
+    window.alert('Dados Inválidos')
+  }
 });
 let photoShareCheckBox = document.getElementById('photo-check');
 photoShareCheckBox.addEventListener('click', () => {
