2f44cad804c80ea45eb59f104508bae5fafcca54
Victor Mendes <victorutdsim7@gmail.com>
2022-07-07 18:31:50 -0300

    requisito bonus 1

--- Modulo 1 - Fundamentos/Bloco 6 - HTML e CSS: Forms, flexbox e responsivo/Dia 1 - Forms/Ex - pratica/script.js
+++ Modulo 1 - Fundamentos/Bloco 6 - HTML e CSS: Forms, flexbox e responsivo/Dia 1 - Forms/Ex - pratica/script.js
@@ -2,3 +2,11 @@ const submitButton = document.getElementById('submit-button');
 submitButton.addEventListener('click', (event) => {
   event.preventDefault();
 });
+let photoShareCheckBox = document.getElementById('photo-check');
+photoShareCheckBox.addEventListener('click', () => {
+  if (photoShareCheckBox.checked) {
+    document.getElementById('insert-photo').style.display = 'block';
+  } else {
+    document.getElementById('insert-photo').style.display = 'none';
+  }
+});
