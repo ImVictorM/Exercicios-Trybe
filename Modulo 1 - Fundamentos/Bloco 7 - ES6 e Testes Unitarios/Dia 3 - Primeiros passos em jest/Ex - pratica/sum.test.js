11a5253d50ad9eff0b3faebd0cab4d299be84a4d
Victor Mendes <victorutdsim7@gmail.com>
2022-07-18 19:41:02 -0300

    [REQ 1] - requisito completado

--- Modulo 1 - Fundamentos/Bloco 7 - ES6 e Testes Unitarios/Dia 3 - Primeiros passos em jest/Ex - pratica/sum.test.js
+++ Modulo 1 - Fundamentos/Bloco 7 - ES6 e Testes Unitarios/Dia 3 - Primeiros passos em jest/Ex - pratica/sum.test.js
@@ -4,4 +4,13 @@ describe('The function sum: ', () => {
   it('Returns 9 when sum(4, 5)', () => {
     expect(sum(4, 5)).toEqual(9);
   });
+
+  it('Returns 0 when sum(0, 0)', () => {
+    expect(sum(0, 0)).toEqual(0);
+  });
+
+  it('Throws an error when sum(4, "5")', () => {
+    expect(() => sum(4, '5')).toThrow('parameters must be numbers');
+  });
+
 })
