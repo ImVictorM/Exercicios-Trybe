d4790b2035e88ce44e119289c0476efd82a8aad2
Victor Mendes <victorutdsim7@gmail.com>
2022-07-21 15:54:58 -0300

    exercicios pratica: exercicio 1

---
+++ Modulo 1 - Fundamentos/Bloco 8 - Higher Order Functions ES6/Dia 1 - Introducao a Higher Order Functions/Ex - pratica/script.js
@@ -0,0 +1,19 @@
+// ex 1
+const employee = (fullName) => {
+  const emailFormat = fullName.split(' ').join('_');
+  return {
+    nomeCompleto: fullName,
+    email: `${emailFormat}@trybe.com`,
+  };
+};
+
+const newEmployees = (func) => {
+  const employees = {
+    id1: func('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: func('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: func('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(employee));
