b564be8f38a616c7ca68b25f2c51375f1d5ae175
Victor Mendes <victorutdsim7@gmail.com>
2022-08-19 15:44:41 -0300

    exercicio de vincular funcoes a classe com this e bind

--- Modulo 2 - Desenvolvimento Front-end/Bloco 11 - Componentes com estado, eventos e formularios com react/Dia 1 - componentes com estados e eventos/Exercicios conteudo/fancy-buttons/src/App.js
+++ Modulo 2 - Desenvolvimento Front-end/Bloco 11 - Componentes com estado, eventos e formularios com react/Dia 1 - componentes com estados e eventos/Exercicios conteudo/fancy-buttons/src/App.js
@@ -1,25 +1,34 @@
 import './App.css';
 import React, { Component } from 'react';
 
-function pastelUva() {
-  console.log('pastel de uva');
-}
-
-function pastelMorango() {
-  console.log('pastel de morango');
-}
-
-function pastelLaranja() {
-  console.log('pastel de laranja');
-}
-
 class App extends Component {
+  constructor() {
+    super();
+    this.pastelLaranja = this.pastelLaranja.bind(this);
+    this.pastelMorango = this.pastelMorango.bind(this);
+    this.pastelUva = this.pastelUva.bind(this);
+  }
+
+  pastelLaranja() {
+    console.log('pastel de laranja');
+    console.log(this);
+  }
+
+  pastelMorango() {
+    console.log('pastel de morango');
+    console.log(this);
+  }
+
+  pastelUva() {
+    console.log('pastel de uva');
+    console.log(this);
+  }
   render() {
     return (
       <section>
-        <button onClick={pastelUva}>Click</button>
-        <button onClick={pastelLaranja}>Click</button>
-        <button onClick={pastelMorango}>Click</button>
+        <button onClick={this.pastelUva}>Click</button>
+        <button onClick={this.pastelLaranja}>Click</button>
+        <button onClick={this.pastelMorango}>Click</button>
       </section>
     );
   }
