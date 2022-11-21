a78708e717f5247ff8b60e2d737c1b60090d66b6
Victor Mendes <victorutdsim7@gmail.com>
2022-11-21 20:57:17 -0300

    feat: resolução exercicios de pratica [bonus]

--- Modulo 3 - Desenvolvimento Back-end/Bloco 20 - Introducao a SQL/Dia 4 - Manipulando tabelas/Ex - pratica/manipulating_pixar_data.sql
+++ Modulo 3 - Desenvolvimento Back-end/Bloco 20 - Introducao a SQL/Dia 4 - Manipulando tabelas/Ex - pratica/manipulating_pixar_data.sql
@@ -58,3 +58,28 @@ WHERE movie_id IN (2, 9);
 
 DELETE FROM pixar.movies
 WHERE director LIKE 'Andrew Stanton';
+
+# ex - 8
+SELECT * FROM pixar.box_office
+ORDER BY rating DESC;
+
+UPDATE pixar.box_office
+SET rating = 9.0
+WHERE domestic_sales > 400000000;
+
+# ex - 9
+UPDATE pixar.box_office
+SET rating = 6.0
+WHERE (international_sales < 300000000)
+AND (domestic_sales > 200000000);
+
+# ex - 10
+SELECT * FROM pixar.movies
+WHERE length_minutes < 100;
+
+DELETE FROM pixar.box_office
+WHERE movie_id IN (1, 6, 7, 8);
+
+DELETE FROM pixar.movies
+WHERE length_minutes < 100;
+
