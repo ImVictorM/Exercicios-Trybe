81f375e68fe6cf871aec780348d4e0589c7b6d6c
Victor Mendes <victorutdsim7@gmail.com>
2022-11-23 15:36:56 -0300

    resolução exercicios de manipular strings

---
+++ Modulo 3 - Desenvolvimento Back-end/Bloco 21 - Funcoes SQL, JOINs e Normalizacao/Dia 1 - Funcoes mais usadas no SQL/Exercicios conteudo/manipulating_strings.sql
@@ -0,0 +1,14 @@
+# ex - 1
+SELECT UCASE('trybe');
+
+# ex - 2
+SELECT REPLACE('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google');
+
+# ex - 3
+SELECT CHAR_LENGTH('Uma frase qualquer');
+
+# ex - 4
+SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', 13, 10);
+
+# ex - 5
+SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
