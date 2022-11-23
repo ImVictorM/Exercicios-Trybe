f5767e149f47f319297d2ffa6dd1627e70c5ad4f
Victor Mendes <victorutdsim7@gmail.com>
2022-11-23 19:23:50 -0300

    resolve desafios extras de MOD e DIV

--- Modulo 3 - Desenvolvimento Back-end/Bloco 21 - Funcoes SQL, JOINs e Normalizacao/Dia 1 - Funcoes mais usadas no SQL/Exercicios conteudo/math_queries.sql
+++ Modulo 3 - Desenvolvimento Back-end/Bloco 21 - Funcoes SQL, JOINs e Normalizacao/Dia 1 - Funcoes mais usadas no SQL/Exercicios conteudo/math_queries.sql
@@ -1,3 +1,14 @@
+# Desafios com DIV e MOD
+# ex - 1
+SELECT IF(15 MOD 2 = 0, 'Par', 'Ímpar') AS 'Par ou Ímpar';
+
+# ex - 2
+SELECT 220 DIV 12;
+
+# ex - 3
+SELECT IF (220 MOD 12 != 0, CONCAT('Sim ', 220 MOD 12, ' lugares sobrando'), 'Não');
+
+# Para fixar
 # ex - 1
 SELECT FLOOR(RAND() * (20-15+1) + 15);
 
