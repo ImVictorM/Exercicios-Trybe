bd50d3c52431a07764d34b8d6690498d4fe419e8
Victor Mendes <victorutdsim7@gmail.com>
2022-12-08 12:41:21 -0300

    [ex-pratica]: req 5 - GET route to get a movie by id

--- Modulo 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/Dia 2 - API REST com Express/Ex - pratica/src/app.js
+++ Modulo 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/Dia 2 - API REST com Express/Ex - pratica/src/app.js
@@ -3,9 +3,21 @@ const { readFile } = require('./utils/fileService');
 
 const app = express();
 
-async function main() {
-  console.log(await readFile('src/movies.json'));
-}
+app.use(express.json());
 
-main();
+app.get('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  const movies = await readFile('src/movies.json');
+  const reqMovie = movies.find((movie) => Number(movie.id) === Number(id));
+  if (!reqMovie) {
+    return res.status(404).json({ error: 'Not Found'});
+  }
+  return res.status(200).json(reqMovie);
+});
+
+// async function main() {
+//   console.log(await readFile('src/movies.json'));
+// }
+
+// main();
 module.exports = app;
