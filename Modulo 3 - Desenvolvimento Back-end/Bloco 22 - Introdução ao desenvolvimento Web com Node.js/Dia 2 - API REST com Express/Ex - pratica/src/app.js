b72390a7f142b731bfe85d8cb3e29edcffded342
Victor Mendes <victorutdsim7@gmail.com>
2022-12-08 12:46:39 -0300

    [ex-pratica]: req 6 - GET route to get the whole list of  movies

--- Modulo 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/Dia 2 - API REST com Express/Ex - pratica/src/app.js
+++ Modulo 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/Dia 2 - API REST com Express/Ex - pratica/src/app.js
@@ -6,18 +6,36 @@ const app = express();
 app.use(express.json());
 
 app.get('/movies/:id', async (req, res) => {
-  const { id } = req.params;
-  const movies = await readFile('src/movies.json');
-  const reqMovie = movies.find((movie) => Number(movie.id) === Number(id));
-  if (!reqMovie) {
-    return res.status(404).json({ error: 'Not Found'});
+  try {
+    const { id } = req.params;
+    const movies = await readFile('src/movies.json');
+    const reqMovie = movies.find((movie) => Number(movie.id) === Number(id));
+    if (!reqMovie) {
+      return res.status(404).json({ error: 'Not Found'});
+    }
+    return res.status(200).json(reqMovie);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: {
+      status: 500,
+      description: 'Internal Server Error',
+    }});
   }
-  return res.status(200).json(reqMovie);
+  
 });
 
-// async function main() {
-//   console.log(await readFile('src/movies.json'));
-// }
+app.get('/movies', async (req, res) => {
+  try {
+    const movies = await readFile('src/movies.json');
+    return res.status(200).json(movies);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: {
+      status: 500,
+      description: 'Internal Server Error',
+    }});
+  } 
+});
 
-// main();
+
 module.exports = app;
