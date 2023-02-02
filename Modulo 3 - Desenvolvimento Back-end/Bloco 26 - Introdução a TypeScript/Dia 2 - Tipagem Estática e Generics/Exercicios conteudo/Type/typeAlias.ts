a578dbfc2c25ece307f3bf16e12634f95a601b13
victor <victorutdsim7@gmail.com>
2023-02-02 14:12:33 -0300

    exercicio conteudo: type aliases

---
+++ Modulo 3 - Desenvolvimento Back-end/Bloco 26 - Introdução a TypeScript/Dia 2 - Tipagem Estática e Generics/Exercicios conteudo/Type/typeAlias.ts
@@ -0,0 +1,13 @@
+type bird = {
+  especies: string,
+  description: string,
+  liveExpectancy: number,
+};
+
+type sum = (n1: number, n2:number ) => number; 
+
+type address = {
+  number: number,
+  city: string,
+  state: string
+}
