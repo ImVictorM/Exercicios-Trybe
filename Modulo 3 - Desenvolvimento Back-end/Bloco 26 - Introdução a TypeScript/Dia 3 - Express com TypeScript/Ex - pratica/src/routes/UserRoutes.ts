545418937caf1c91da911f04c2a9a35c5c0380e8
victor <victorutdsim7@gmail.com>
2023-02-04 15:26:02 -0300

    exercicio pratica: CRUD /users

--- Modulo 3 - Desenvolvimento Back-end/Bloco 26 - Introdução a TypeScript/Dia 3 - Express com TypeScript/Ex - pratica/src/routes/UserRoutes.ts
+++ Modulo 3 - Desenvolvimento Back-end/Bloco 26 - Introdução a TypeScript/Dia 3 - Express com TypeScript/Ex - pratica/src/routes/UserRoutes.ts
@@ -6,5 +6,9 @@ const UserRouter = express.Router();
 const userController = new UserController();
 
 UserRouter.get('/', userController.requestAll);
+UserRouter.get('/:id', userController.requestOne);
+UserRouter.post('/', userController.requestCreateOne);
+UserRouter.put('/:id', userController.requestUpdateOne);
+UserRouter.delete('/:id', userController.requestDeleteOne);
 
 export default UserRouter;
--- Modulo 3 - Desenvolvimento Back-end/Bloco 26 - Introdução a TypeScript/Dia 3 - Express com TypeScript/Ex - pratica/src/services/UserService.ts
+++ Modulo 3 - Desenvolvimento Back-end/Bloco 26 - Introdução a TypeScript/Dia 3 - Express com TypeScript/Ex - pratica/src/services/UserService.ts
@@ -12,4 +12,23 @@ export default class UserService {
     const users = await this.userModel.findAll();
     return users;
   };
+
+  getOneInteraction = async (id: number): Promise<IUser> => {
+    const user = await this.userModel.findOne(id);
+    return user;
+  }
+
+  createOneInteraction = async (newUser: IUser): Promise<IUser> => {
+    const createdUser = await this.userModel.createOne(newUser);
+    return createdUser;
+  }
+
+  updateOneInteraction = async (id:number, updatedInfo: Partial<IUser>): Promise<IUser> => {
+    const updatedUser = await this.userModel.updateOne(id, updatedInfo);
+    return updatedUser;
+  }
+
+  deleteOneInteraction = async (id: number): Promise<void> => {
+    await this.userModel.deleteOne(id);
+  }
 }
