0945eae9ab4b809a3949c8cdd167908b1d4b5167
Victor Mendes <victorutdsim7@gmail.com>
2023-02-09 15:53:59 -0300

    exercicio conteudo: sintaxe de encapsulamento

--- Modulo 3 - Desenvolvimento Back-end/Bloco 27 - POO e SOLID/Dia 1 - Introducao a Orientacao a Objetos/Exercicios conteudo/Tv.ts
+++ Modulo 3 - Desenvolvimento Back-end/Bloco 27 - POO e SOLID/Dia 1 - Introducao a Orientacao a Objetos/Exercicios conteudo/Tv.ts
@@ -1,9 +1,9 @@
 class tv {
-  private brand: string;
-  private size: number;
-  private resolution: string;
-  private connections: string[];
-  private connectedTo: string | null = null;
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo: string | null = null;
 
   constructor(
     brand: string, 
@@ -11,16 +11,33 @@ class tv {
     resolution: string, 
     connections: string[]
   ) {
-    this.brand = brand;
-    this.size = size;
-    this.resolution = resolution;
-    this.connections = connections;
+    this._brand = brand;
+    this._size = size;
+    this._resolution = resolution;
+    this._connections = connections;
   }
 
   public turnOn = () => {
-    console.log(this.brand, this.size, this.resolution, this.connections);
+    console.log(this._brand, this._size, this._resolution, this._connections);
+  }
+
+  public get connectedTo(): string | null {
+    return this._connectedTo;
+  }
+
+  public set connectedTo(value: string | null) {
+    if (!value || this._connections.includes(value)) {
+      this._connectedTo = value;
+    } else {
+      console.log('Sorry, connection unavailable!');
+    }
   }
 };
 
 const myTv = new tv('ASUS', 32, 'Full HD', ['HDMI']);
 myTv.turnOn();
+console.log(myTv.connectedTo);
+myTv.connectedTo = 'HDMI';
+console.log(myTv.connectedTo);
+myTv.connectedTo = 'Bluetooth';
+console.log(myTv.connectedTo);
