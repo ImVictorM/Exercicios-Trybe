d0aff20b833d6e26e8d32336d3d56bbeb97ab138
Victor Mendes <victorutdsim7@gmail.com>
2023-02-14 16:55:10 -0300

    exercicios conteudo: open/closed principle

--- Modulo 3 - Desenvolvimento Back-end/Bloco 27 - POO e SOLID/Dia 4 - SOLID - Introducao e Principios S, O e D/Exercicios conteudo/src/index.ts
+++ Modulo 3 - Desenvolvimento Back-end/Bloco 27 - POO e SOLID/Dia 4 - SOLID - Introducao e Principios S, O e D/Exercicios conteudo/src/index.ts
@@ -6,10 +6,15 @@ type Discipline = {
   letterGrade?: string;
 };
 
+type School = {
+  name: string;
+  approvalGrade: number;
+};
+
 type Student = {
   name: string;
   disciplines: Discipline[];
-  school?: string;
+  school: School; // Agora não é mais uma string
 };
 
 /* Apoio para a função `getGradeLetter` */
@@ -41,8 +46,8 @@ const percentageGradesIntoLetters = (student: Student): Student => ({
 });
 
 /* "Determinar" */
-const approvedStudents = ({ disciplines }: Student): boolean =>
-  disciplines.every(({ grade }) => grade > 0.7);
+const approvedStudents = ({ disciplines, school }: Student): boolean =>
+  disciplines.every(({ grade }) => grade >= school.approvalGrade);
 
 /* "Atualizar" */
 const updateApprovalData = (student: Student): void => {
@@ -58,6 +63,27 @@ function setApproved(students: Student[]): void {
     .filter(approvedStudents)
     .map(updateApprovalData);
 }
+
+const studentsExample = [
+  {
+    name: 'Lee',
+    school: { name: 'Standard', approvalGrade: 0.7 },
+    disciplines: [
+      { name: 'matemática', grade: 0.8 },
+      { name: 'história', grade: 0.9 },
+    ],
+  },
+  {
+    name: 'Albus',
+    school: { name: 'Hogwarts', approvalGrade: 0.8 },
+    disciplines: [
+      { name: 'divination', grade: 0.8 },
+      { name: 'potions', grade: 0.9 },
+    ],
+  },
+];
+
+setApproved(studentsExample);
 
 export {
   percentageGradesIntoLetters,
