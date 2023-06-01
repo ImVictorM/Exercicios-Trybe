ee2b34559dced06615800757a0f29c57aea47bea
Victor Mendes <victorutdsim7@gmail.com>
2023-06-01 17:19:04 -0300

    dia 3 - exercicio 1

---
+++ Modulo 4 - Ciencia Da Computacao/Bloco 36 - Estrutura de dados I/Dia 3 - Arrays/Ex - pratica/ex_1.py
@@ -0,0 +1,25 @@
+# complexidade: O(n)
+def calc_stability(results: list[bool]):
+    max_time_without_instabilities = 0
+
+    left = 0
+    right = left + 1
+    occurrences = 1
+
+    while left <= max_time_without_instabilities:
+        if results[left] == results[right]:
+            right += 1
+            occurrences += 1
+        else:
+            left = right
+            right = left + 1
+            occurrences = 1
+
+        if occurrences > max_time_without_instabilities:
+            max_time_without_instabilities = occurrences
+
+    return max_time_without_instabilities
+
+
+print(calc_stability([0, 1, 1, 1, 0, 0, 1, 1]))
+print(calc_stability([1, 1, 1, 1, 0, 0, 1, 1]))
